5ed04bd6cfbbd429e750003dec293303e27f01c4
Nyulas <52659959+Nyulas@users.noreply.github.com>
2019-11-13 09:39:42 +0200

    Add files via upload
    basic look of the Calendar without any feature

---
+++ Calendar.NETDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Calendar.NET;
+
+namespace Calendar.NETDemo
+{
+    public partial class Form1 : Form
+    {
+        [CustomRecurringFunction("RehabDates", "Calculates which days I should be getting Rehab")]
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            calendar1.CalendarDate = new DateTime(2012, 5, 2, 0, 0, 0);
+            calendar1.CalendarView = CalendarViews.Month;
+            calendar1.AllowEditingEvents = true;
+
+            
+        }
+    }
+}
