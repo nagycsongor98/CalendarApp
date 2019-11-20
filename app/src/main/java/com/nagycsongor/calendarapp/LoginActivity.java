ac92a1abbc56b1d82f6d8fcd52d6fc0da6c21593
Arni10 <arnycadabra@gmail.com>
2019-11-20 15:54:06 +0200

    Login activity now working to

--- app/src/main/java/com/nagycsongor/calendarapp/LoginActivity.java
+++ app/src/main/java/com/nagycsongor/calendarapp/LoginActivity.java
@@ -1,28 +1,46 @@
 package com.nagycsongor.calendarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    private TextView emailTextView;
-    private TextView passwordTextView;
+    private TextView emailEditText;
+    private TextView passwordEditText;
     private Button loginButton;
     private TextView registerTextView;
-
+    private ProgressBar progressBar;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        emailTextView = findViewById(R.id.loginEmailEditText);
-        passwordTextView = findViewById(R.id.loginPasswordEditText);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        emailEditText = findViewById(R.id.loginEmailEditText);
+        passwordEditText = findViewById(R.id.loginPasswordEditText);
         loginButton = findViewById(R.id.loginLoginButton);
         registerTextView = findViewById(R.id.loginRegisterTextView);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +56,63 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login(){
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+
+        final String email = emailEditText.getText().toString().trim();
+        final String password = passwordEditText.getText().toString().trim();
+
+        if (email.isEmpty())
+        {
+            emailEditText.setError("Email field is empty!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            emailEditText.setError("Email form not correct!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty())
+        {
+            passwordEditText.setError("Password field is empty!");
+            passwordEditText.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.GONE);
+        myRef.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean is = false;
+                for(DataSnapshot userSnapshot : dataSnapshot.getChildren()){
+                    User userExist = userSnapshot.getValue(User.class);
+                    if (userExist.getEmail().equals(email) && userExist.getPassword().equals(password)){
+                        progressBar.setVisibility(View.VISIBLE);
+                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        is = true;
+                        break;
+                    }
+                }
+                if (!is){
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), "Email and password not match", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        progressBar.setVisibility(View.GONE);
     }
 }
 
--- app/src/main/java/com/nagycsongor/calendarapp/RegisterActivity.java
+++ app/src/main/java/com/nagycsongor/calendarapp/RegisterActivity.java
@@ -27,8 +27,6 @@ public class RegisterActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private Button registerButton;
     private TextView loginTextView;
-   // private FirebaseAuth mAuth;
-    private String TAG = "RegisterActivity";
     private FirebaseDatabase database;
     private DatabaseReference myRef;
 
@@ -73,16 +71,13 @@ public class RegisterActivity extends AppCompatActivity {
         final String email = emailEditText.getText().toString().trim();
         final String name = nameEditText.getText().toString().trim();
         final String password = passwordEditText.getText().toString().trim();
-        //String passwordEncrypted = PasswordEncryption.encrypt(password);
         String passwordAgain = passwordAgainEditText.getText().toString().trim();
-        //String passwordAgainEncrypted = PasswordEncryption.encrypt(passwordAgain);
 
         if (email.isEmpty())
         {
             emailEditText.setError("Email field is empty!");
             emailEditText.requestFocus();
             return;
-            //Toast.makeText(RegisterActivity.this, "Email field is empty!", Toast.LENGTH_SHORT).show();
         }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
@@ -90,7 +85,6 @@ public class RegisterActivity extends AppCompatActivity {
             emailEditText.setError("Email form not correct!");
             emailEditText.requestFocus();
             return;
-            //Toast.makeText(RegisterActivity.this, "Email form not correct!", Toast.LENGTH_SHORT).show();
         }
 
         if (name.isEmpty())
@@ -98,7 +92,6 @@ public class RegisterActivity extends AppCompatActivity {
             nameEditText.setError("Name field is empty!");
             nameEditText.requestFocus();
             return;
-            //Toast.makeText(RegisterActivity.this, "Name field is empty!", Toast.LENGTH_SHORT).show();
         }
 
         if (password.isEmpty())
@@ -106,7 +99,6 @@ public class RegisterActivity extends AppCompatActivity {
             passwordEditText.setError("Password field is empty!");
             passwordEditText.requestFocus();
             return;
-            //Toast.makeText(RegisterActivity.this, "Password field is empty!", Toast.LENGTH_SHORT).show();
         }
 
         if (passwordAgain.isEmpty())
@@ -114,7 +106,6 @@ public class RegisterActivity extends AppCompatActivity {
             passwordAgainEditText.setError("Passeord again field is empty!");
             passwordAgainEditText.requestFocus();
             return;
-            //Toast.makeText(RegisterActivity.this, "Passeord again field is empty!", Toast.LENGTH_SHORT).show();
         }
 
         if (!password.equals(passwordAgain))
@@ -122,7 +113,6 @@ public class RegisterActivity extends AppCompatActivity {
             passwordAgainEditText.setError("The two passwords doesn't match");
             passwordAgainEditText.requestFocus();
             return;
-            //Toast.makeText(RegisterActivity.this, "The two passwords doesn't match", Toast.LENGTH_SHORT).show();
         }
 
 
@@ -133,12 +123,14 @@ public class RegisterActivity extends AppCompatActivity {
                 for(DataSnapshot userSnapshot : dataSnapshot.getChildren()){
                     User userExist = userSnapshot.getValue(User.class);
                     if (userExist.getEmail().equals(email)){
+                        progressBar.setVisibility(View.GONE);
                         Toast.makeText(RegisterActivity.this, "This email already exists!", Toast.LENGTH_SHORT).show();
                         is = true;
                         break;
                     }
                 }
                 if (!is){
+                    progressBar.setVisibility(View.VISIBLE);
                     String key = myRef.child("Users").push().getKey();
                     User user = new User(email, name, password);
                     myRef.child("Users").child(key).setValue(user);
@@ -154,5 +146,10 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        progressBar.setVisibility(View.GONE);
     }
 }
